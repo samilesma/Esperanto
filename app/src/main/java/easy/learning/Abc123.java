218a2bb6487126b7bca91f99ad2449e19b3fe847
Mikfor <Mik>
2016-11-21 12:03:43 +0100

    Knap med man er klar
    Den henviser fra learning til test

--- app/src/main/java/easy/learning/Abc123.java
+++ app/src/main/java/easy/learning/Abc123.java
@@ -6,10 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.example.esperanto.R;
 
+import easy.testing.Abc123test;
 
 
 public class Abc123 extends Fragment implements View.OnClickListener {
@@ -17,6 +19,7 @@ public class Abc123 extends Fragment implements View.OnClickListener {
     private TextView tLearn1,tLearn2,tLearn3,tLearn4;
     private ImageView ivLearn1, ivLearn2, ivLearn3,ivLearn4;
     private ImageView ivSound1, ivSound2, ivSound3, ivSound4;
+    private Button bReady;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,11 +36,19 @@ public class Abc123 extends Fragment implements View.OnClickListener {
         ivLearn3 = (ImageView) show.findViewById(R.id.ivLearn3);
         ivLearn4 = (ImageView) show.findViewById(R.id.ivLearn4);
 
+        bReady = (Button) show.findViewById(R.id.bReady);
+
         return show;
     }
 
     @Override
     public void onClick(View v) {
+        if(v==bReady){
+            getFragmentManager().beginTransaction().setCustomAnimations(android.R.anim.slide_in_left,android.R.anim.slide_out_right)
+                    .replace(R.id.fragmentindhold, new Abc123()).addToBackStack(null).commit();
+
+        }
+
 
     }
 }
