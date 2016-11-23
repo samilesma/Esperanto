905f658a1e4f51e1f4dfe0c8b9d57cb87510a379
Mikfor <Mik>
2016-11-23 15:34:47 +0100

    Hente ord
    Har nok brug for hjælp med at hente billeder. Ved ikke om den er rigtig

--- app/src/main/java/com/example/esperanto/Controller.java
+++ app/src/main/java/com/example/esperanto/Controller.java
@@ -2,7 +2,15 @@ package com.example.esperanto;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.support.v4.app.FragmentActivity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Controller {
     SharedPreferences level;
@@ -18,4 +26,32 @@ public class Controller {
         this.level.edit().putInt("level", level).apply();
         this.level.edit().putInt("underLevel", underLevel).apply();
     }
+
+    public static String getUrl(String url) throws IOException {
+        URL quickconnect = new URL(url);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        quickconnect.openStream()));
+
+        String full="";
+        String inputLine;
+
+        while ((inputLine = in.readLine()) != null) full=full+"\n"+inputLine;
+
+        in.close();
+
+        return full;
+
+    }
+
+    public static Drawable LoadImageFromWebOperations(String url) {
+        try {
+            InputStream inputstream = (InputStream) new URL(url).getContent();
+            Drawable d = Drawable.createFromStream(inputstream, "src name");
+            return d;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
 }
