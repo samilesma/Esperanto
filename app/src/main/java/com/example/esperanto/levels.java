e6969ae517b24e6036b47b7b7e602aab1838d6d5
ahma0942 <ahmad.almajdi@yahoo.dk>
2016-11-28 12:36:43 +0100

    asd

--- app/src/main/java/com/example/esperanto/levels.java
+++ app/src/main/java/com/example/esperanto/levels.java
@@ -5,12 +5,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by ahmad on 23-11-2016.
  */
 
 public class levels extends Fragment {
+    private TextView tBeginner, tIntermediate, tExpert;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
