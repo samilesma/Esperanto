6cdd66c543ae9e15e12e318a34d4b60da19eaba4
Mikfor <Mik>
2017-01-10 14:28:18 +0100

    retter fejl

--- app/src/main/java/com/example/esperanto/Category_frag.java
+++ app/src/main/java/com/example/esperanto/Category_frag.java
@@ -1,16 +1,12 @@
 package com.example.esperanto;
 
-import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
-import gamemodes.DragAnddrop_frag;
 import gamemodes.Fourpic_frag;
 
 /**
