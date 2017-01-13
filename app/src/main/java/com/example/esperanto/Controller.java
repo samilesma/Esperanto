22831d13a106c353896c2af35e6316e584ad45f2
ahma0942 <ahmad.almajdi@yahoo.dk>
2017-01-13 15:23:58 +0100

    axc

--- app/src/main/java/com/example/esperanto/Controller.java
+++ app/src/main/java/com/example/esperanto/Controller.java
@@ -1,5 +1,6 @@
 package com.example.esperanto;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v4.app.FragmentActivity;
 import org.json.JSONArray;
@@ -24,17 +25,13 @@ public class Controller {
     public static JSONObject json=null;
 
     public Controller(FragmentActivity activity) {
-        /*
-        level = activity.getSharedPreferences(
-                "level", Context.MODE_PRIVATE);
-        level = activity.getSharedPreferences(
-                "underLevel", Context.MODE_PRIVATE);
-                */
+//        level = activity.getSharedPreferences("levelType",Context.MODE_PRIVATE);
+//        level = activity.getSharedPreferences("underLevel",Context.MODE_PRIVATE);
     }
 
     public void setNiveau(int level, int underLevel){
-        this.level.edit().putInt("level", level).apply();
-        this.level.edit().putInt("underLevel", underLevel).apply();
+        this.level.edit().putInt("level",level).apply();
+        this.level.edit().putInt("underLevel",underLevel).apply();
     }
 
     public void selectLevel(String type, int lvl) throws JSONException, ExecutionException, InterruptedException {
