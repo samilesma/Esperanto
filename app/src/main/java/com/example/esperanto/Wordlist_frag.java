9909623cdefdc03a65b4d268f0ee33966ea77a8a
samilesma <samilesma@gmail.com>
2017-01-18 17:46:18 +0100

    Listview

--- app/src/main/java/com/example/esperanto/Wordlist_frag.java
+++ app/src/main/java/com/example/esperanto/Wordlist_frag.java
@@ -2,40 +2,69 @@ package com.example.esperanto;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class Wordlist_frag extends Fragment {
 
-    private ListView list;
+    LinearLayout lLay;
+
+    // Array of strings for ListView Title
+    String[] listviewTitle = new String[]{
+            "ListView Title 1", "ListView Title 2", "ListView Title 3", "ListView Title 4",
+    };
+
+    Button b;
+
+
+    int[] listviewImage = new int[]{
+            R.drawable.back, R.drawable.domo, R.drawable.elefanto, R.drawable.glass,
+    };
+
+    String[] listviewShortDescription = new String[]{
+            "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description",
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.wordlist_frag, container, false);
 
-        list = (ListView) view.findViewById(R.id.list);
+        lLay = (LinearLayout) view.findViewById(R.id.lLay);
 
-        ArrayList<String> text = new ArrayList<>();
+        List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
 
-        text.add("foo");
-        text.add("bar");
+        b = new Button(getActivity().getApplicationContext());
+        b.setText("a");
+        b.setTextSize(TypedValue.COMPLEX_UNIT_PX, 10);
+        lLay.addView(b);
 
-        // This is the array adapter, it takes the context of the activity as a
-        // first parameter, the type of list view as a second parameter and your
-        // array as a third parameter.
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                getActivity(),
-                android.R.layout.simple_list_item_1,
-                text );
+        for (int i = 0; i < 4; i++) {
+            HashMap<String, String> hm = new HashMap<String, String>();
+            hm.put("listview_title", listviewTitle[i]);
+            hm.put("listview_discription", listviewShortDescription[i]);
+            hm.put("listview_image", Integer.toString(listviewImage[i]));
+            aList.add(hm);
+        }
 
-        list.setAdapter(arrayAdapter);
+        String[] from = {"listview_image", "listview_title", "listview_discription"};
+        int[] to = {R.id.listview_image, R.id.listview_item_title, R.id.listview_item_short_description};
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(getActivity(), aList, R.layout.listview_activity, from, to);
+        ListView androidListView = (ListView) view.findViewById(R.id.list);
+        androidListView.setAdapter(simpleAdapter);
 
         return view;
     }
